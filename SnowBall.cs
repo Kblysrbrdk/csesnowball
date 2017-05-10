465c509ba6621205356f365c6ea2a9842acf27cc
Kblysrbrdk <turquoise.kubilayy@gmail.com>
2017-05-10 22:13:41 +0300

    Add files via upload

---
+++ SnowBall.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowBall : MonoBehaviour {
+
+    public float ballSpeed;
+
+    private Rigidbody2D theRB;
+
+    public GameObject snowBallEffect;
+	
+	void Start () {
+        theRB = GetComponent<Rigidbody2D>();
+	}
+	
+	
+	void Update () {
+        theRB.velocity = new Vector2(ballSpeed * transform.localScale.x, 0);
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player1") {
+            FindObjectOfType<GameManager>().HurtP1();
+        }
+        if (other.tag == "Player2")
+        {
+            FindObjectOfType<GameManager>().HurtP2();
+        }
+
+        Instantiate(snowBallEffect, transform.position, transform.rotation);
+
+        Destroy(gameObject);
+    }
+}
