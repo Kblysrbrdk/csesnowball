61726ce649f575e212778cd88cc365557266211c
Kblysrbrdk <turquoise.kubilayy@gmail.com>
2017-05-10 21:26:00 +0300

    Add files via upload

---
+++ ChooseMap.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChooseMap : MonoBehaviour {
+	public string easyMapScene;
+	public string difficultMapScene;
+	public string backMenuScene;
+
+	public void EasyMap()
+	{
+		SceneManager.LoadScene(easyMapScene);
+
+
+	}
+	public void DifficultMap()
+	{
+		
+		SceneManager.LoadScene(difficultMapScene);
+
+	}
+	public void BackMenu()
+	{
+
+		SceneManager.LoadScene(backMenuScene);
+
+	}
+
+}
